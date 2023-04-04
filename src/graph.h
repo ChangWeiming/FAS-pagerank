d4fd99e8f6113ec417620abbe65eddbee32b93a3
forBlank <1422848728@qq.com>
2023-04-04 11:21:07 +0800

    update LineGraph

--- src/graph.h
+++ src/graph.h
@@ -1,7 +1,11 @@
+#ifndef __GRAPH_H__
+#define __GRAPH_H__
+
 #include <vector>
 #include <cstring>
 
 using namespace std;
+
 struct Edge {
     int from, to, next;
     Edge (int f = 0, int t = 0, int n = -1) {
@@ -10,22 +14,60 @@ struct Edge {
 };
 
 class Graph {
-public:
-    Graph(int _n) {
-        n = _n;
-        head = vector<int> (n+1, -1);
-        p = vector<double> (n+1, 0);
-    }
-    void addedge(int from, int to) {
-        edges.push_back(Edge(from, to, head[from]));
-        head[from] = edges.size() - 1;
-    }
-    //图中的点的个数
-    int n;
-    //head数组
-    vector<int> head;
-    //边集合数组
-    vector<Edge> edges;
-    //pagerank概率
-    vector<double> p;
+    public:
+        Graph(int _n) {
+            n = _n;
+            head = vector<int> (n+1, -1);
+        }
+
+        virtual void add_edge(int from, int to) {
+            edges.push_back(Edge(from, to, head[from]));
+            head[from] = edges.size() - 1;
+        }
+        // Number of Node
+        int n;
+        // Node Set
+        vector<int> head;
+        // Edge Set
+        vector<Edge> edges;
 };
+
+class LineGraph: public Graph {
+    public:
+        LineGraph(int _n): Graph(_n) {
+            p = vector<float> (n+1, 0);
+            p_out = vector<float> (n+1, 0);
+            in_degree = vector<int> (n+1, 0);
+            out_degree = vector<int> (n+1, 0);
+        }
+
+        void add_edge(int from, int to) {
+            edges.push_back(Edge(from, to, head[from]));
+            head[from] = edges.size() - 1;
+            in_degree[to] += 1;
+		    out_degree[from] += 1;
+        }
+
+        int get_max_p_index() {
+            int node_num = head.size()-1;
+            float max_p = 0;
+            int index = 0;
+            for(int i = 1; i <= node_num; i++) {
+                if(max_p < p[i]) {
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        // Node's Indegree
+    	vector<int> in_degree;
+        // Node's Outdegree
+        vector<int> out_degree;  
+        // Node's Page Rank Score
+        vector<float> p;
+        // Node's Page Rank Score Out
+        vector<float> p_out;
+}; 
+
+#endif
--- src/page_rank.cpp
+++ src/page_rank.cpp
@@ -1,60 +1,18 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <cstring>
 #include <cmath>
+#include "graph.h"
+#include "utils.h"
 
 using namespace std;
 
-struct Edge {
-    int from, to, next;
-    Edge (int f = 0, int t = 0, int n = -1) {
-        from = f; to = t; next = n;
-    }
-};
-
-class Graph {
-public:
-    Graph(int _n) {
-        n = _n;
-        head = vector<int> (n+1, -1);
-        p = vector<float> (n+1, 0);
-		p_out = vector<float> (n+1, 0);
-        in_degree = vector<int> (n+1, 0);
-        out_degree = vector<int> (n+1, 0);
-    }
-    void addedge(int from, int to) {
-        edges.push_back(Edge(from, to, head[from]));
-        head[from] = edges.size() - 1;
-        in_degree[to] += 1;
-		out_degree[from] += 1;
-    }
-    //图中的点的个数
-    int n;
-    //head数组
-    vector<int> head;
-	//入度数组
-	vector<int> in_degree;
-	//出度数组
-	vector<int> out_degree;  
-    //边集合数组
-    vector<Edge> edges;
-    //Page Rank Score
-    vector<float> p;
-    //Page Rank Score Out
-    vector<float> p_out;
-};
-
-void print_page_rank(Graph* g) {
+void print_page_rank(LineGraph* g) {
 	int node_num = g->head.size()-1;
 	for (int i = 1; i <= node_num; i++)
         cout << "Vertex " << i << " page rank score = " << g->p[i] << endl;
 	cout << endl;
 }
 
-void page_rank(Graph* g, int max_iteration) {
+void page_rank(LineGraph* g, int max_iteration) {
 	
 	int node_num = g->head.size()-1;
 	float init_score = 1.0 / node_num;
@@ -63,7 +21,6 @@ void page_rank(Graph* g, int max_iteration) {
 	
 	print_page_rank(g);
 	
-	float diff = 1.0;
 	int iteration = 1;
 	while(iteration < max_iteration) {
 		
@@ -72,14 +29,12 @@ void page_rank(Graph* g, int max_iteration) {
 				g->p_out[i] = g->p[i] / g->in_degree[i];
 		}
 		
-		diff = 0;
 		for(int i = 1; i <= node_num; i++) {
 			float p_sum = 0;
 			for(int j = g->head[i]; j != -1; j = g->edges[j].next) {
 				int to = g->edges[j].to;
 				p_sum += g->p_out[to];
 			}
-			diff += fabs(g->p[i] - p_sum);
 			g->p[i] = p_sum;
 		}
 		// print_page_rank(g);
@@ -94,27 +49,12 @@ int main() {
     
     int n = 6;
     
-    Graph lineGraph(n);
+    LineGraph lineGraph(n);
     
     string file_path = "./dataset.txt";
-    ifstream file(file_path.c_str());
-    if (!file.is_open()) {
-        cerr << "Error opening file.\n";
-        return 1;
-    }
 
-    string line;
-    while (getline(file, line)) {
-        stringstream ss(line);
-        int num1, num2;
-        char comma;
-        if (ss >> num1 >> comma >> num2) {
-            lineGraph.addedge(num2, num1);
-        }
-    }
-
-    file.close();
-
+    readGraph(&lineGraph, file_path);
+    
     for (int i = 1; i <= lineGraph.head.size()-1; i++) {
         cout << "Vertex " << i << " is connected to: ";
         for (int j = lineGraph.head[i]; j != -1; j = lineGraph.edges[j].next) {
@@ -126,13 +66,9 @@ int main() {
     }
     
     page_rank(&lineGraph, 20);
-    
-    
 
-    // cout << "FAS is:\n";
-    // for(int i = lineGraph.head[x]; i != -1; i = g.edges[i].next) {
-    //     cout << x << ' ' << g.edges[i].to << endl;
-    // }
+    int index = lineGraph.get_max_p_index();
+    cout << "Max page rank score index = " << index << endl;
 
     return 0;
 }
